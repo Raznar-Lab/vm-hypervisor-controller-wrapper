42fe22f2b199f9c0d500191c2a14cc7e41a2ea0f
unknown <xabhista19@gmail.com>
2025-03-18 19:48:21 +0700

    feat: update mac addr

--- interfaces/vm/vm_request/vm_install_os.go
+++ interfaces/vm/vm_request/vm_install_os.go
@@ -1,5 +1,6 @@
 package vm_request
 
 type VMInstallOSRequestData struct {
-	ImageFile     string `json:"image_file"`
+	ImageFile string `json:"image_file"`
+	MacAddr   string `json:"mac_address"`
 }
